c7a8b6112dbee0a0e7c4f0c48469d19d9816df61
Anderson Grudtner Martins <anderson@ostraining.com>
2017-01-17 19:37:41 -0200

    Rename the plugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,10 @@ $_tests_dir = '/tmp/wordpress/tests/phpunit';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../edit_flow.php';
+	require dirname( __FILE__ ) . '/../publishpress.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
-require dirname( __FILE__ ) . '/testcase-edit-flow-ajax.php';
+require dirname( __FILE__ ) . '/testcase-publishpress-ajax.php';
