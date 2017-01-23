50197b18dea2cc9bed49d51182877a893bf18503
Anderson Grudtner Martins <anderson@ostraining.com>
2017-01-23 19:17:36 -0200

    Minor fix on the white-space for tests

--- tests/test-publishpress-class-module.php
+++ tests/test-publishpress-class-module.php
@@ -30,94 +30,94 @@
 
 class WP_Test_PublishPress_Class_Module extends WP_UnitTestCase {
 
-	protected static $admin_user_id;
-	protected static $PublishPressModule;
+    protected static $admin_user_id;
+    protected static $PublishPressModule;
 
-	public static function wpSetUpBeforeClass($factory) {
-		self::$admin_user_id = $factory->user->create(array('role' => 'administrator'));
-	}
+    public static function wpSetUpBeforeClass($factory) {
+        self::$admin_user_id = $factory->user->create(array('role' => 'administrator'));
+    }
 
-	function _flush_roles() {
-		// we want to make sure we're testing against the db, not just in-memory data
-		// this will flush everything and reload it from the db
-		unset($GLOBALS['wp_user_roles']);
-		global $wp_roles;
-		if (is_object($wp_roles)) {
-			$wp_roles->_init();
-		}
-	}
+    function _flush_roles() {
+        // we want to make sure we're testing against the db, not just in-memory data
+        // this will flush everything and reload it from the db
+        unset($GLOBALS['wp_user_roles']);
+        global $wp_roles;
+        if (is_object($wp_roles)) {
+            $wp_roles->_init();
+        }
+    }
 
-	function setUp() {
-		parent::setUp();
+    function setUp() {
+        parent::setUp();
 
-		self::$PublishPressModule = new PP_Module();
+        self::$PublishPressModule = new PP_Module();
 
-		$this->_flush_roles();
-	}
+        $this->_flush_roles();
+    }
 
-	function tearDown() {
-		self::$PublishPressModule = null;
-	}
+    function tearDown() {
+        self::$PublishPressModule = null;
+    }
 
-	function test_add_caps_to_role() {
-		$usergroup_roles = array(
-			'administrator' => array('edit_usergroups'),
-		);
+    function test_add_caps_to_role() {
+        $usergroup_roles = array(
+            'administrator' => array('edit_usergroups'),
+        );
 
-		foreach($usergroup_roles as $role => $caps) {
-			self::$PublishPressModule->add_caps_to_role($role, $caps);
-		}
+        foreach($usergroup_roles as $role => $caps) {
+            self::$PublishPressModule->add_caps_to_role($role, $caps);
+        }
 
-		$user = new WP_User(self::$admin_user_id);
+        $user = new WP_User(self::$admin_user_id);
 
-		//Verify before flush
-		$this->assertTrue($user->has_cap('edit_usergroups'), 'User did not have role edit_usergroups');
+        //Verify before flush
+        $this->assertTrue($user->has_cap('edit_usergroups'), 'User did not have role edit_usergroups');
 
-		$this->_flush_roles();
+        $this->_flush_roles();
 
-		$this->assertTrue($user->has_cap('edit_usergroups'), 'User did not have role edit_usergroups');
-	}
+        $this->assertTrue($user->has_cap('edit_usergroups'), 'User did not have role edit_usergroups');
+    }
 
-	function test_current_post_type_post_type_set() {
-		$_REQUEST['post_type'] = 'not-real';
+    function test_current_post_type_post_type_set() {
+        $_REQUEST['post_type'] = 'not-real';
 
-		$this->assertEquals('not-real', self::$PublishPressModule->get_current_post_type());
-	}
+        $this->assertEquals('not-real', self::$PublishPressModule->get_current_post_type());
+    }
 
-	function test_current_post_type_post_screen() {
-		set_current_screen('post.php');
+    function test_current_post_type_post_screen() {
+        set_current_screen('post.php');
 
-		$post_id = $this->factory->post->create(array (
-			'post_author' => self::$admin_user_id
-		));
+        $post_id = $this->factory->post->create(array (
+            'post_author' => self::$admin_user_id
+        ));
 
-		$_REQUEST['post'] = $post_id;
+        $_REQUEST['post'] = $post_id;
 
-		$this->assertEquals('post', self::$PublishPressModule->get_current_post_type());
+        $this->assertEquals('post', self::$PublishPressModule->get_current_post_type());
 
-		unset($_REQUEST['post_type']);
-		set_current_screen('front');
-	}
+        unset($_REQUEST['post_type']);
+        set_current_screen('front');
+    }
 
-	function test_current_post_type_edit_screen() {
-		set_current_screen('edit.php');
+    function test_current_post_type_edit_screen() {
+        set_current_screen('edit.php');
 
-		$this->assertEquals('post', self::$PublishPressModule->get_current_post_type());
+        $this->assertEquals('post', self::$PublishPressModule->get_current_post_type());
 
-		set_current_screen('front');
-	}
+        set_current_screen('front');
+    }
 
-	function test_current_post_type_custom_post_type() {
-		register_post_type('content');
-		set_current_screen('content');
+    function test_current_post_type_custom_post_type() {
+        register_post_type('content');
+        set_current_screen('content');
 
-		$this->assertEquals('content', self::$PublishPressModule->get_current_post_type());
+        $this->assertEquals('content', self::$PublishPressModule->get_current_post_type());
 
-		_unregister_post_type('content');
-		set_current_screen('front');
-	}
+        _unregister_post_type('content');
+        set_current_screen('front');
+    }
 
-	public static function wpTearDownAfterClass() {
-		self::delete_user(self::$admin_user_id);
-	}
+    public static function wpTearDownAfterClass() {
+        self::delete_user(self::$admin_user_id);
+    }
 }
--- tests/test-publishpress-dashboard-notepad.php
+++ tests/test-publishpress-dashboard-notepad.php
@@ -30,25 +30,25 @@
 
 class WP_Test_PublishPress_Dashboard_Note extends WP_UnitTestCase {
 
-	function test_register_dashboard_note_post_type() {
-		//As part of the Edit Flow initialziation process
-		//PP_Dashboard_Notepad_Widget should have already
-		//created the dashboard-note post type
-		$pobj = get_post_type_object('dashboard-note');
-		$this->assertEquals('dashboard-note', $pobj->name);
+    function test_register_dashboard_note_post_type() {
+        //As part of the Edit Flow initialziation process
+        //PP_Dashboard_Notepad_Widget should have already
+        //created the dashboard-note post type
+        $pobj = get_post_type_object('dashboard-note');
+        $this->assertEquals('dashboard-note', $pobj->name);
 
-		//Testing PP_Dashboard_Notepad_Widget::init explicitly
-		_unregister_post_type('dashboard-note');
+        //Testing PP_Dashboard_Notepad_Widget::init explicitly
+        _unregister_post_type('dashboard-note');
 
-		$PublishPressDashboardNote = new PP_Dashboard_Notepad_Widget();
+        $PublishPressDashboardNote = new PP_Dashboard_Notepad_Widget();
 
-		$this->assertNull(get_post_type_object('dashboard-note'));
+        $this->assertNull(get_post_type_object('dashboard-note'));
 
-		//Should create the post type 'dashboard-note'
-		$PublishPressDashboardNote->init();
+        //Should create the post type 'dashboard-note'
+        $PublishPressDashboardNote->init();
 
-		$pobj = get_post_type_object('dashboard-note');
-		$this->assertEquals('dashboard-note', $pobj->name);
-	}
+        $pobj = get_post_type_object('dashboard-note');
+        $this->assertEquals('dashboard-note', $pobj->name);
+    }
 
 }
