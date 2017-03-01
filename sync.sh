c1bb7f42282f9d478465f32ad0efe3b979f18261
Anderson Grudtner Martins <anderson@ostraining.com>
2017-03-01 15:16:16 -0300

    Add script to sync local dev folder with wordpress

---
+++ sync.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Sync the src folder into wordpress, to auto-update the code while 
+# developing. Used with fswatch in Mac OS to detect file changes.
+
+rsync -rtvu --delete ./src ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-3
