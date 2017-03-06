27db571367ea95a525b3d7be78e1cd7ef77d3026
Anderson Grudtner Martins <anderson@ostraining.com>
2017-03-06 18:09:53 -0300

    Fixes the plugin folder

--- sync-src.sh
+++ sync-src.sh
@@ -3,6 +3,6 @@
 # Sync the src folder into wordpress, to auto-update the code while 
 # developing. Used with fswatch in Mac OS to detect file changes.
 
-# rsync -rtvu --delete ./src ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-3
+# rsync -rtvu --delete ./src ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress
 rm -rf ./src/*
-cp -R ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-3/* ./src/
+cp -R ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress/* ./src/
