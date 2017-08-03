44c5191795b19f96a626cbf1faee5562162a51f5
Anderson Grudtner Martins <anderson@ostraining.com>
2017-08-03 12:44:26 -0300

    Updates version to 1.7.0a1

--- src/publishpress.php
+++ src/publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PressShack
  * Author URI: https://pressshack.com
- * Version: 1.6.2b2
+ * Version: 1.7.0a1
  *
  * Copyright (c) 2017 PressShack
  *
