9068683a3e02d40849969388ad81e9d417426a74
Anderson Grudtner Martins <anderson@ostraining.com>
2017-10-05 15:27:03 -0300

    Bump alpha version

--- src/publishpress.php
+++ src/publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PressShack
  * Author URI: https://pressshack.com
- * Version: 1.7.5a1
+ * Version: 1.7.5a2
  *
  * Copyright (c) 2017 PressShack
  *
