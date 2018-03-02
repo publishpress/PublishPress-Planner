0f5a0d260b3b66bc0b2d8945c36619901707438b
Anderson Grudtner Martins <anderson@ostraining.com>
2018-03-02 12:45:23 -0300

    Prepare to release 1.10.1-beta2

--- includes.php
+++ includes.php
@@ -41,7 +41,7 @@ if (!defined('PP_LOADED'))
     );
 
     // Define contants
-    define('PUBLISHPRESS_VERSION', '1.10.1-beta1');
+    define('PUBLISHPRESS_VERSION', '1.10.1-beta2');
     define('PUBLISHPRESS_ROOT', dirname(__FILE__));
     define('PUBLISHPRESS_FILE_PATH', PUBLISHPRESS_ROOT . '/' . basename(__FILE__));
     define('PUBLISHPRESS_URL', plugins_url('/', __FILE__));
