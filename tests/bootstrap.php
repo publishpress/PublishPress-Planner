cc31043bfade25f90b89d69d759242aebf50e9fc
Anderson Grudtner Martins <anderson@ostraining.com>
2018-03-14 02:54:44 -0300

    Test adding the addons

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,6 +11,9 @@ if ( ! $_tests_dir ) {
     $_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
+$_wp_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress';
+$_plugins_dir = $_wp_dir . '/wp-content/plugins';
+
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
     echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
     exit( 1 );
@@ -24,6 +27,11 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
     require dirname( dirname( __FILE__ ) ) . '/publishpress.php';
+
+    // Check if there are plugins available
+    if (file_exists($_plugins_dir . '/publishpress-permissions')) {
+        require $_plugins_dir . '/publishpress-permissions/publishpress-permissions.php';
+    }
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
