0275588be599a011c8d5d8ab4b1113d70687473b
Anderson Grudtner Martins <anderson@ostraining.com>
2018-03-28 21:03:55 -0300

    Bump beta version

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.11.4-beta2
+ * Version: 1.11.4-beta3
  * Text Domain: publishpress
  * Domain Path: /languages
  *
