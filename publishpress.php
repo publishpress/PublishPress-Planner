04180e820511757d3b92a483d5cacf9845b7352f
Anderson Grudtner Martins <anderson@ostraining.com>
2018-04-06 11:36:54 -0300

    Bump minor version as alpha1

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.11.4
+ * Version: 1.12.0-alpha1
  * Text Domain: publishpress
  * Domain Path: /languages
  *
