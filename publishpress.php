10b06137618453c5ee44ec1043352ca01f22f9f9
Anderson Grudtner Martins <anderson@alledia.com>
2018-09-26 16:42:50 -0300

    Bump patch and beta version to 1.16.4-beta.1

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.16.3
+ * Version: 1.16.4-beta.1
  * Text Domain: publishpress
  * Domain Path: /languages
  *
