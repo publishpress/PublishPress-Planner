d55cbd5f8a11ced8818a4200b8e54bec30132f3b
Anderson Grudtner Martins <anderson@alledia.com>
2018-12-04 11:40:59 -0200

    Bump version to 1.18.0-beta.2

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.17.1-beta.3
+ * Version: 1.18.0-beta.2
  *
  * Copyright (c) 2018 PublishPress
  *
