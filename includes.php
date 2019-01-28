b355280f09617a00eecce91e6ad91013552301ae
andergmartins <anderson@alledia.com>
2019-01-28 16:17:13 -0200

    Bump patch and beta version

--- includes.php
+++ includes.php
@@ -40,7 +40,7 @@ if ( ! defined('PP_LOADED')) {
     );
 
     // Define contants
-    define('PUBLISHPRESS_VERSION', '1.18.4');
+    define('PUBLISHPRESS_VERSION', '1.18.5-beta.1');
     define('PUBLISHPRESS_BASE_PATH', __DIR__);
     define('PUBLISHPRESS_FILE_PATH', PUBLISHPRESS_BASE_PATH . '/' . basename(__FILE__));
     define('PUBLISHPRESS_URL', plugins_url('/', __FILE__));
--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.18.4
+ * Version: 1.18.5-beta.1
  *
  * Copyright (c) 2018 PublishPress
  *
