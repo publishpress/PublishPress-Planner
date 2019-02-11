9f7458d200dd013a406074299ce1c56064db9b78
andergmartins <anderson@alledia.com>
2019-02-11 16:57:26 -0200

    Bump minor version to 1.19.0

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.18.6-beta.3
+ * Version: 1.19.0
  *
  * Copyright (c) 2018 PublishPress
  *
