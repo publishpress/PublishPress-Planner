ebe7690e9b69c05f6af7873e5d9b4d635598fd40
andergmartins <anderson@alledia.com>
2019-02-15 14:34:10 -0200

    Replace the custom-status-block.js file with a ES5 version

--- modules/custom-status/lib/custom-status-block.js
+++ modules/custom-status/lib/custom-status-block.js
@@ -6,38 +6,39 @@
  * Copyright (c) 2009-2019 Mohammad Jangda, Daniel Bachhuber, et al.
  * ------------------------------------------------------------------------------
  */
-let {
-  __
-} = wp.i18n;
-let {
-  PluginPostStatusInfo
-} = wp.editPost;
-let {
-  registerPlugin
-} = wp.plugins;
-let {
-  withSelect,
-  withDispatch
-} = wp.data;
-let {
-  compose
-} = wp.compose;
-let {
-  SelectControl
-} = wp.components;
+var __ = wp.i18n.__;
+var PluginPostStatusInfo = wp.editPost.PluginPostStatusInfo;
+var registerPlugin = wp.plugins.registerPlugin;
+var _wp$data = wp.data,
+    withSelect = _wp$data.withSelect,
+    withDispatch = _wp$data.withDispatch;
+var compose = wp.compose.compose;
+var SelectControl = wp.components.SelectControl;
 /**
  * Map Custom Statuses as options for SelectControl
  */
 
-let statuses = window.PPCustomStatuses.map(s => ({
-  label: s.name,
-  value: s.slug
-}));
+var statuses = window.PPCustomStatuses.map(function (s) {
+  return {
+    label: s.name,
+    value: s.slug
+  };
+});
 
-let getStatusLabel = slug => statuses.find(s => s.value === slug).label; // Remove the Published status from the list.
+var getStatusLabel = function getStatusLabel(slug) {
+  var item = statuses.find(function (s) {
+    return s.value === slug;
+  });
+
+  if (label) {
+    return item.label;
+  }
+
+  return '';
+}; // Remove the Published status from the list.
 
 
-statuses = statuses.filter(item => {
+statuses = statuses.filter(function (item) {
   return item.value !== 'publish';
 });
 /**
@@ -48,21 +49,21 @@ statuses = statuses.filter(item => {
  * @param status
  */
 
-let sideEffectL10nManipulation = status => {
+var sideEffectL10nManipulation = function sideEffectL10nManipulation(status) {
   if (status === 'publish') {
     return;
   }
 
-  let statusLabel = getStatusLabel(status);
-  setTimeout(() => {
-    let node = document.querySelector('.editor-post-save-draft');
+  var statusLabel = getStatusLabel(status);
+  setTimeout(function () {
+    var node = document.querySelector('.editor-post-save-draft');
 
     if (!node) {
       node = document.querySelector('.editor-post-switch-to-draft');
     }
 
     if (node) {
-      document.querySelector('.editor-post-save-draft, .editor-post-switch-to-draft').innerText = `${__('Save as')} ${statusLabel}`;
+      document.querySelector('.editor-post-save-draft, .editor-post-switch-to-draft').innerText = "".concat(__('Save as'), " ").concat(statusLabel);
       node.dataset.ppInnerTextUpdated = true;
     }
   }, 100);
@@ -75,22 +76,22 @@ let sideEffectL10nManipulation = status => {
  */
 
 
-setInterval(() => {
-  let status = wp.data.select('core/editor').getEditedPostAttribute('status');
+setInterval(function () {
+  var status = wp.data.select('core/editor').getEditedPostAttribute('status');
 
   if (status === 'publish') {
     return;
   }
 
-  let statusLabel = getStatusLabel(status);
-  let node = document.querySelector('.editor-post-save-draft');
+  var statusLabel = getStatusLabel(status);
+  var node = document.querySelector('.editor-post-save-draft');
 
   if (!node) {
     node = document.querySelector('.editor-post-switch-to-draft');
   }
 
   if (node && !node.dataset.ppInnerTextUpdated) {
-    document.querySelector('.editor-post-save-draft, .editor-post-switch-to-draft').innerText = `${__('Save as')} ${statusLabel}`;
+    document.querySelector('.editor-post-save-draft, .editor-post-switch-to-draft').innerText = "".concat(__('Save as'), " ").concat(statusLabel);
     node.dataset.ppInnerTextUpdated = true;
   }
 }, 250);
@@ -99,33 +100,38 @@ setInterval(() => {
  * @param object props
  */
 
-let PPCustomPostStatusInfo = ({
-  onUpdate,
-  status
-}) => wp.element.createElement(PluginPostStatusInfo, {
-  className: `publishpress-extended-post-status publishpress-extended-post-status-${status}`
-}, wp.element.createElement("h4", null, __('Post Status', 'publishpress')), status !== 'publish' ? wp.element.createElement(SelectControl, {
-  label: "",
-  value: status,
-  options: statuses,
-  onChange: onUpdate
-}) : __('Published', 'publishpress'), wp.element.createElement("small", {
-  className: "publishpress-extended-post-status-note"
-}, status !== 'publish' ? __(`Note: this will override all status settings above.`, 'publishpress') : __('To select a custom status, please unpublish the content first.', 'publishpress')));
+var PPCustomPostStatusInfo = function PPCustomPostStatusInfo(_ref) {
+  var onUpdate = _ref.onUpdate,
+      status = _ref.status;
+  return wp.element.createElement(PluginPostStatusInfo, {
+    className: "publishpress-extended-post-status publishpress-extended-post-status-".concat(status)
+  }, wp.element.createElement("h4", null, __('Post Status', 'publishpress')), status !== 'publish' ? wp.element.createElement(SelectControl, {
+    label: "",
+    value: status,
+    options: statuses,
+    onChange: onUpdate
+  }) : wp.element.createElement("div", null, __('Published', 'publishpress')), wp.element.createElement("small", {
+    className: "publishpress-extended-post-status-note"
+  }, status !== 'publish' ? __("Note: this will override all status settings above.", 'publishpress') : __('To select a custom status, please unpublish the content first.', 'publishpress')));
+};
 
-let plugin = compose(withSelect(select => ({
-  status: select('core/editor').getEditedPostAttribute('status')
-})), withDispatch(dispatch => ({
-  onUpdate(status) {
-    dispatch('core/editor').editPost({
-      status
-    });
-    sideEffectL10nManipulation(status);
-  }
-
-})))(PPCustomPostStatusInfo);
+var plugin = compose(withSelect(function (select) {
+  return {
+    status: select('core/editor').getEditedPostAttribute('status')
+  };
+}), withDispatch(function (dispatch) {
+  return {
+    onUpdate: function onUpdate(status) {
+      dispatch('core/editor').editPost({
+        status: status
+      });
+      sideEffectL10nManipulation(status);
+    }
+  };
+}))(PPCustomPostStatusInfo);
 registerPlugin('publishpress-custom-status-block', {
   icon: 'admin-site',
   render: plugin
 });
+
 //# sourceMappingURL=custom-status-block.js.map
