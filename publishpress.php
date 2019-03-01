c03e816f7001533625e788aa813df1c67ab24016
andergmartins <anderson@alledia.com>
2019-03-01 16:49:04 -0300

    Bump beta version to 1.19.5-beta.2

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.19.5-beta.1
+ * Version: 1.19.5-beta.2
  *
  * Copyright (c) 2018 PublishPress
  *
