d46bd68073d8d4d24c51947fe43408f67f170581
andergmartins <anderson@alledia.com>
2019-04-10 15:33:49 -0300

    Bump patch and alpha version to 1.20.1-alpha.1

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: The essential plugin for any WordPress site with multiple writers
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.20.0
+ * Version: 1.20.1-alpha.1
  *
  * Copyright (c) 2018 PublishPress
  *
