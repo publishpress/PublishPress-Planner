3dd5fdcb305a5abd9fa0d55dc8c06d02d07f8fc1
Alex Bouma <me@alexbouma.me>
2019-05-09 10:16:38 +0200

    Exclude taxonomies on slug when retrieving terms

--- libraries/Notifications/Workflow/Step/Event_Content/Filter/Term.php
+++ libraries/Notifications/Workflow/Step/Event_Content/Filter/Term.php
@@ -57,42 +57,32 @@ class Term extends Base implements Filter_Interface
      */
     protected function get_options()
     {
-        $get_terms_to_exclude = '';
+        $get_terms_to_exclude = [];
 
-        if (class_exists('\PP_Notifications')) {
-            $blacklisted_taxonomies = \PP_Notifications::getOption('blacklisted_taxonomies');
-            if (!empty($blacklisted_taxonomies)) {
-                $blacklisted_taxonomies = array_filter(explode(',', $blacklisted_taxonomies));
-                if (count($blacklisted_taxonomies) > 0) {
-                    $get_terms_to_exclude = get_terms([
-                        'fields'   => 'ids',
-                        'taxonomy' => $blacklisted_taxonomies,
-                    ]);
-
-                    if (is_wp_error($get_terms_to_exclude)) {
-                        $get_terms_to_exclude = '';
-                    }
-                }
+        if ( class_exists( '\PP_Notifications' ) ) {
+            $blacklisted_taxonomies = \PP_Notifications::getOption( 'blacklisted_taxonomies' );
+            if ( ! empty( $blacklisted_taxonomies ) ) {
+                $get_terms_to_exclude = array_filter( explode( ',', $blacklisted_taxonomies ) );
             }
         }
 
-        $terms = get_terms([
+        $terms = get_terms( [
             'hide_empty' => false,
-            'exclude'    => $get_terms_to_exclude,
-        ]);
+            'taxonomy'   => array_except( get_taxonomies(), $get_terms_to_exclude ),
+        ] );
 
-        $metadata = (array)$this->get_metadata(static::META_KEY_TERM);
+        $metadata = (array) $this->get_metadata( static::META_KEY_TERM );
 
         $options = [];
-        foreach ($terms as $term) {
+        foreach ( $terms as $term ) {
             $options[] = [
                 'value'    => $term->term_id,
                 'label'    => $term->taxonomy . '/' . $term->name,
-                'selected' => in_array($term->term_id, $metadata),
+                'selected' => in_array( $term->term_id, $metadata ),
             ];
         }
 
-        usort($options, [$this, 'sort_options']);
+        usort( $options, [ $this, 'sort_options' ] );
 
         return $options;
     }
