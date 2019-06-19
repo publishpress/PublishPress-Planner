52db8b2f9e243c33b8bdc98240fb2be502d98fd6
Anderson Grudtner Martins <anderson@alledia.com>
2019-06-19 12:39:26 -0300

    Add the logo to the notifications screen

--- modules/settings/settings.php
+++ modules/settings/settings.php
@@ -212,9 +212,7 @@ if ( ! class_exists('PP_Settings')) {
 
 			<div class="publishpress-admin pressshack-admin-wrapper wrap">
 				<header>
-                    <div class="logo-header">
-                        <img src="<?php echo PUBLISHPRESS_URL . '/common/img/publishpress-logo-icon.png';?>" alt="" class="logo-header" />
-                    </div>
+                    <img src="<?php echo PUBLISHPRESS_URL . '/common/img/publishpress-logo-icon.png';?>" alt="" class="logo-header" />
 
 					<h1 class="wp-heading-inline"><?php echo $current_module->title; ?></h1>
 
