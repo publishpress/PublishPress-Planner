6301a952f318d132ddeb882447a988e93b0e311f
Anderson Grudtner Martins <anderson@alledia.com>
2019-09-11 15:15:19 -0300

    Update version number to 1.20.9

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: PublishPress helps you plan and publish content with WordPress. Features include a content calendar, notifications, and custom statuses.
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.20.9-beta.7
+ * Version: 1.20.9
  *
  * Copyright (c) 2019 PublishPress
  *
