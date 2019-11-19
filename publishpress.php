95b01c7d0189637b7b1893d10f9fe248f0ffb1e2
Anderson Grudtner Martins <anderson@alledia.com>
2019-11-19 11:47:06 -0300

    Bump to stable version 1.21.2 and update the changelo

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: PublishPress helps you plan and publish content with WordPress. Features include a content calendar, notifications, and custom statuses.
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 1.21.2-alpha.5
+ * Version: 1.21.2
  *
  * Copyright (c) 2019 PublishPress
  *
