af48006e0b262fa947ce461eb38a5a446c1d4777
Anderson Grudtner Martins <anderson@publishpress.com>
2020-03-23 21:50:15 -0300

    Bump version to 2.0.4-alpha.3

--- includes.php
+++ includes.php
@@ -40,7 +40,7 @@ if (!defined('PP_LOADED')) {
     );
 
     // Define contants
-    define('PUBLISHPRESS_VERSION', '2.0.4-alpha.2');
+    define('PUBLISHPRESS_VERSION', '2.0.4-alpha.3');
     define('PUBLISHPRESS_BASE_PATH', __DIR__);
     define('PUBLISHPRESS_FILE_PATH', PUBLISHPRESS_BASE_PATH . DIRECTORY_SEPARATOR . 'publishpress.php');
     define('PUBLISHPRESS_URL', plugins_url('/', __FILE__));
