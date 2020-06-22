e5bfd72872b5aa00cd7964b05c02b95490d62162
Anderson Grudtner Martins <anderson@publishpress.com>
2020-06-22 20:21:37 -0300

    Add initial version of Robo file

---
+++ RoboFile.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package     PublishPress
+ * @author      PublishPress
+ * @copyright   Copyright (C) 2020 PublishPress. All rights reserved.
+ */
+
+class RoboFile extends \PublishPressBuilder\Tasks
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+//        $this->appendToFileToIgnore(
+//            [
+//
+//            ]
+//        );
+    }
+}
