03cf78312f551cb0fbe0e3e5db6e529885c8b8ff
Anderson Grudtner Martins <anderson@publishpress.com>
2020-10-08 17:50:29 -0300

    Update version

--- includes.php
+++ includes.php
@@ -41,7 +41,7 @@ if (!defined('PP_LOADED')) {
     );
 
     // Define constants
-    define('PUBLISHPRESS_VERSION', '2.3.0');
+    define('PUBLISHPRESS_VERSION', '2.3.1-hotfix-692-visual-composer');
     define('PUBLISHPRESS_BASE_PATH', __DIR__);
     define('PUBLISHPRESS_FILE_PATH', PUBLISHPRESS_BASE_PATH . '/publishpress.php');
     define('PUBLISHPRESS_URL', plugins_url('/', __FILE__));
--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: PublishPress helps you plan and publish content with WordPress. Features include a content calendar, notifications, and custom statuses.
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 2.3.0
+ * Version: 2.3.1-hotfix-692-visual-composer
  * Text Domain: publishpress
  * Domain Path: /languages
  *
