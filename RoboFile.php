dffb2189c439a71ad0a196c5b5dc1fa79db7c820
Anderson Grudtner Martins <anderson@publishpress.com>
2020-10-13 12:41:54 -0300

    Fix the robo file

--- RoboFile.php
+++ RoboFile.php
@@ -28,27 +28,6 @@ class RoboFile extends \PublishPressBuilder\PackageBuilderTasks
         parent::__construct();
 
         $this->setVersionConstantName('PUBLISHPRESS_VERSION');
-        $this->appendToFileToIgnore(
-            [
-                'webpack.config.js',
-                'legacy-tests',
-                'src/assets/lib/chosen-v1.8.3/docsupport',
-                'src/assets/lib/chosen-v1.8.3/bower.json',
-                'src/assets/lib/chosen-v1.8.3/create-example.jquery.html',
-                'src/assets/lib/chosen-v1.8.3/create-example.proto.html',
-                'src/assets/lib/chosen-v1.8.3/index.proto.html',
-                'src/assets/lib/chosen-v1.8.3/options.html',
-                'src/assets/lib/chosen-v1.8.3/package.json',
-                'vendor/pimple/pimple/.gitignore',
-                'vendor/pimple/pimple/.php_cs.dist',
-                'vendor/psr/container/.gitignore',
-                'vendor/publishpress/wordpress-version-notices/.gitignore',
-                'vendor/publishpress/wordpress-version-notices/README.md',
-                'vendor/publishpress/wordpress-version-notices/bin',
-                'vendor/publishpress/wordpress-version-notices/codeception.dist.yml',
-                'vendor/publishpress/wordpress-version-notices/codeception.yml',
-                'vendor/publishpress/wordpress-version-notices/tests',
-            ]
-        );
+        $this->setVersionConstantFiles(['includes.php']);
     }
 }
