7072270934cb30e0739661d0d756581c1ebb8567
Anderson Grudtner Martins <anderson@publishpress.com>
2020-11-12 14:18:15 -0300

    Update the version number

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: PublishPress helps you plan and publish content with WordPress. Features include a content calendar, notifications, and custom statuses.
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 2.4.2
+ * Version: 2.4.2-feature-727-post-type-filter-content-overview.1
  * Text Domain: publishpress
  * Domain Path: /languages
  *
