dc4fc2df0bbf4ae674cb3d5c055377151e7830a5
Anderson Grudtner Martins <anderson@publishpress.com>
2021-05-04 16:17:30 -0300

    Add simple loading message to the calendar

--- modules/calendar/calendar.php
+++ modules/calendar/calendar.php
@@ -1566,7 +1566,11 @@ if (!class_exists('PP_Calendar')) {
 
             </div>
 
-            <div id="publishpress-async-calendar-wrapper"></div>
+            <div id="publishpress-async-calendar-wrapper">
+                <div class="publishpress-async-calendar-loader">
+                    <?php echo __('Please, wait until the calendar is loaded...', 'publishpress'); ?>
+                </div>
+            </div>
             <?php
             $publishpress->settings->print_default_footer($publishpress->modules->calendar);
         }
