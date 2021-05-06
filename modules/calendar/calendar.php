41fc2ddd2fdb17788556d06aaec4f821d5a51300
Anderson Grudtner Martins <anderson@publishpress.com>
2021-05-06 13:10:23 -0300

    Refactor the styles separating the theme

--- modules/calendar/calendar.php
+++ modules/calendar/calendar.php
@@ -467,9 +467,16 @@ if (!class_exists('PP_Calendar')) {
                 );
 
                 wp_enqueue_style(
-                    'publishpress-async-calendar-css',
-                    $this->module_url . 'lib/async-calendar/async-calendar.css',
+                    'publishpress-async-calendar-theme-light-css',
+                    $this->module_url . 'lib/async-calendar/styles/themes/theme-light.min.css',
                     [],
+                    PUBLISHPRESS_VERSION
+                );
+
+                wp_enqueue_style(
+                    'publishpress-async-calendar-css',
+                    $this->module_url . 'lib/async-calendar/styles/async-calendar.min.css',
+                    ['publishpress-async-calendar-theme-light-css'],
                     PUBLISHPRESS_VERSION
                 );
 
