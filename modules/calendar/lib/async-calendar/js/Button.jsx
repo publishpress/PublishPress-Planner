d42350b93d3522b50a841b5137c405f1e84746ba
Anderson Grudtner Martins <anderson@publishpress.com>
2021-05-18 12:22:55 -0300

    Improve layout and design of navigation bar

--- modules/calendar/lib/async-calendar/js/Button.jsx
+++ modules/calendar/lib/async-calendar/js/Button.jsx
@@ -1,5 +1,11 @@
 export default function Button(props) {
+    let icon;
+
+    if (props.icon) {
+        icon = <span className={"dashicons dashicons-" + props.icon}></span>;
+    }
+
     return (
-        <a href={props.href || '#'} className="button" onClick={props.onClick}>{props.label || 'Do it'}</a>
+        <a href={props.href || '#'} className="" onClick={props.onClick}>{icon}{props.label}</a>
     )
 }
