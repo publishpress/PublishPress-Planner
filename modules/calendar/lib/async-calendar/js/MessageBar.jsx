43e5fabb5a8928b0b2337d59622502dc26d6a5a3
Anderson Grudtner Martins <anderson@publishpress.com>
2021-05-18 15:54:57 -0300

    Refactor the spin and loader code

--- modules/calendar/lib/async-calendar/js/MessageBar.jsx
+++ modules/calendar/lib/async-calendar/js/MessageBar.jsx
@@ -5,19 +5,8 @@ export default function MessageBar(props) {
     return (
         <div className={'publishpress-calendar-message-bar'}>
             {showSpinner &&
-                <div className="sk-cube-grid">
-                    <div className="sk-cube sk-cube1"></div>
-                    <div className="sk-cube sk-cube2"></div>
-                    <div className="sk-cube sk-cube3"></div>
-                    <div className="sk-cube sk-cube4"></div>
-                    <div className="sk-cube sk-cube5"></div>
-                    <div className="sk-cube sk-cube6"></div>
-                    <div className="sk-cube sk-cube7"></div>
-                    <div className="sk-cube sk-cube8"></div>
-                    <div className="sk-cube sk-cube9"></div>
-                </div>
+                <span className={'dashicons dashicons-update-alt publishpress-spinner reverse'}/>
             }
-
             <span>{message}</span>
         </div>
     )
