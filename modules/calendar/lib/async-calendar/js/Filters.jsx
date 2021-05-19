bfacdc7a8a0670c41a8da7f283f794a45cc622bd
Anderson Grudtner Martins <anderson@publishpress.com>
2021-05-19 09:55:40 -0300

    Add the post status filter

---
+++ modules/calendar/lib/async-calendar/js/Filters.jsx
@@ -0,0 +1,37 @@
+import Select from "./Select";
+
+const $ = jQuery;
+
+export default function Filters(props) {
+    const statusesOptions = props.statuses.map((item) => {
+        return {
+            value: item.slug,s
+            text: item.name
+        };
+    });
+
+    const handleStatusChange = (e, element, value) => {
+        window.dispatchEvent(
+            new CustomEvent(
+                'PublishpressCalendar:filter',
+                {
+                    detail: {
+                        filter: 'status',
+                        value: $(element).pp_select2('data')
+                    }
+                }
+            )
+        );
+    }
+
+    return (
+        <>
+            <Select
+                blankOptionText={"All statuses"}
+                options={statusesOptions}
+                onSelect={handleStatusChange}
+                onClear={handleStatusChange}
+                />
+        </>
+    )
+}
---
+++ modules/calendar/lib/async-calendar/js/Select.jsx
@@ -0,0 +1,44 @@
+const $ = jQuery;
+
+export default function Select(props) {
+    const selectRef = React.useRef(null);
+
+    const initSelect2 = () => {
+        $(selectRef.current).pp_select2({
+            placeholder: props.blankOptionText || false,
+            tags: true,
+            allowClear: true
+        })
+            .on('select2:select', (e) => {
+                props.onSelect(e, selectRef.current);
+            })
+            .on('select2:clear', (e) => {
+                props.onClear(e, selectRef.current);
+            });
+
+        return () => {
+            $(selectRef.current).pp_select2('destroy');
+        }
+    };
+
+    const blankOption = () => {
+        if (props.blankOptionText) {
+            return <option value="">{props.blankOptionText}</option>
+        }
+
+        return <></>;
+    };
+
+    React.useEffect(initSelect2, []);
+
+    return (
+        <select className="pp_select2"
+                multiple={props.multiple}
+                ref={selectRef}>
+            {blankOption()}
+            {props.options.map((option) => {
+                return <option value={option.value}>{option.text}</option>
+            })}
+        </select>
+    )
+}
