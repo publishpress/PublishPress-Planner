00124413eadef5fd5905337d7ddd464d0b54bafa
Anderson Grudtner Martins <anderson@publishpress.com>
2021-06-01 16:48:07 -0300

    Improve the popup left/right positioning

--- modules/calendar/lib/async-calendar/js/Popup.jsx
+++ modules/calendar/lib/async-calendar/js/Popup.jsx
@@ -4,14 +4,30 @@ export default function Popup(props) {
     const popupRef = React.useRef(null);
     const [top, setTop] = React.useState(0);
     const [left, setLeft] = React.useState(0);
-    const offsetX = 4;
+    const offsetX = 10;
+    const offsetWidth = 300;
 
     const setPosition = () => {
         const targetPosition = $(props.target.current).position();
+        const targetOffset = $(props.target.current).offset();
         const targetWidth = $(props.target.current).width();
+        const popupWidth = $(popupRef.current).width();
+
+        const isWiderThanParentWidth = () => {
+            return (targetOffset.left + popupWidth + offsetX) >= $(document).width() - offsetWidth;
+        }
+
+        const getPositionOnRightSide = () => {
+            return targetPosition.left + targetWidth + offsetX;
+        }
+
+        const getPositionOnLeftSide = () => {
+            return targetPosition.left - (offsetX * 3) - popupWidth;
+        }
 
         setTop(targetPosition.top);
-        setLeft(targetPosition.left + targetWidth + offsetX);
+        setLeft(isWiderThanParentWidth() ? getPositionOnRightSide() : getPositionOnLeftSide());
+
     }
 
     const didMount = () => {
