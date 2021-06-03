bc56cf68339a70f6c37967921b4710bb0f1cec2f
Anderson Grudtner Martins <anderson@publishpress.com>
2021-06-03 15:52:05 -0300

    Do not block clicks on links in the popup

--- modules/calendar/lib/async-calendar/js/CalendarItem.jsx
+++ modules/calendar/lib/async-calendar/js/CalendarItem.jsx
@@ -36,8 +36,6 @@ export default function CalendarItem(props) {
     }
 
     const dispatchClickEvent = (e) => {
-        e.preventDefault();
-
         if (isPopupElementOrChildrenOfPopup(e.target)) {
             return;
         }
