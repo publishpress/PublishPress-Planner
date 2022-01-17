19d77867b9c6f1e9f946e7a828a0da02d83e461f
andergmartins <anderson@publishpress.com>
2022-01-17 10:11:53 -0300

    Escape strings in the menu for the Debug module

--- modules/debug/debug.php
+++ modules/debug/debug.php
@@ -210,7 +210,7 @@ if (! class_exists('PP_Debug')) {
 
             $args = [
                 'id' => 'publishpress_debug',
-                'title' => __('PublishPress Debug Log', 'publishpress'),
+                'title' => esc_html__('PublishPress Debug Log', 'publishpress'),
                 'href' => admin_url('admin.php?page=' . self::PAGE_SLUG),
             ];
 
@@ -222,8 +222,8 @@ if (! class_exists('PP_Debug')) {
             // Admin menu.
             add_submenu_page(
                 admin_url('admin.php?page=' . self::PAGE_SLUG),
-                __('Debug Log'),
-                __('Debug Log'),
+                esc_html__('Debug Log'),
+                esc_html__('Debug Log'),
                 'activate_plugins',
                 'publishpress_debug_log',
                 [$this, 'view_log_page']
