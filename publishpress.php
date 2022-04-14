2c292639648b4e0d34820014de36b613c927d689
andergmartins <anderson@publishpress.com>
2022-04-14 11:54:51 -0300

    Bump version to 3.7.1-beta.3

--- publishpress.php
+++ publishpress.php
@@ -5,7 +5,7 @@
  * Description: PublishPress helps you plan and publish content with WordPress. Features include a content calendar, notifications, and custom statuses.
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 3.7.1-beta.2
+ * Version: 3.7.1-beta.3
  * Text Domain: publishpress
  * Domain Path: /languages
  *
