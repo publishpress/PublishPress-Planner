7008c49c61baa53006ff48ab127221884bd5f0cb
Olawale Adesina <olawale.olatech.pro@gmail.com>
2022-05-31 11:05:26 +0100

    - All Users showing in dropdown #1035

--- modules/calendar/calendar.php
+++ modules/calendar/calendar.php
@@ -2930,6 +2930,7 @@ if (! class_exists('PP_Calendar')) {
             $user_args = [
                 'number' => 20,
                 'orderby' => 'display_name',
+                'capability' => 'edit_posts',
             ];
 
             if (! empty($queryText)) {
