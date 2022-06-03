9b647b9c2702f215581d3c2ab24f6fd0298424f2
Olawale Adesina <olawale.olatech.pro@gmail.com>
2022-06-03 11:03:11 +0100

    - Move content overview search box to top right conner

--- modules/content-overview/lib/content-overview.js
+++ modules/content-overview/lib/content-overview.js
@@ -121,4 +121,18 @@ jQuery(document).ready(function ($) {
       $('#pp-content-filters-hidden').submit();
     });
   
+    //populate hidden dearch input and trigger filter to search
+    $('#co-searchbox-search-submit').on('click', function (e) {
+      e.preventDefault();
+      $('#search_box-search-input').val($('#co-searchbox-search-input').val())
+      $('#filter-submit').trigger('click');
+    });
+    $('#co-searchbox-search-input').on('keyup', function (e) {
+      e.preventDefault();
+      if (e.keyCode === 13) {
+        $('#search_box-search-input').val($('#co-searchbox-search-input').val())
+        $('#filter-submit').trigger('click');
+      }
+    });
+  
 });
