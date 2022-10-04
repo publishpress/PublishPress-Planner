2f7309f96b4ba39ead5f73e98983a9a26e91d504
Anderson Grüdtner Martins <anderson@publishpress.com>
2022-10-04 16:18:04 -0300

    Fix typo

--- views/workflow_metabox_section.html.php
+++ views/workflow_metabox_section.html.php
@@ -1,6 +1,6 @@
 <div id="psppno-workflow-metabox-section-<?php echo esc_attr($context['id']); ?>"
      class="<?php echo esc_attr(isset($context['class']) ? $context['class'] : ''); ?> psppno-workflow-metabox-section">
-    <?if (! empty($context['header'])) : ?>
+    <?php if (! empty($context['header'])) : ?>
         <div class="psppno_workflow_metabox_section_header">
             <?php echo $context['header']; ?>
         </div>
