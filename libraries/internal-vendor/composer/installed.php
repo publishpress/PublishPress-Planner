bf15e64a24abb8a6f7702ba7f98eb2ad13e53992
Olawale Adesina <olawale.olatech.pro@gmail.com>
2023-06-20 15:15:20 +0100

    - Update the version number to the next stable version

--- libraries/internal-vendor/composer/installed.php
+++ libraries/internal-vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3cbf9e3df1f424f92b21ad926d44828707f32837',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3cbf9e3df1f424f92b21ad926d44828707f32837',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
