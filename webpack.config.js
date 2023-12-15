ca409e6a15943763617562c2aa13897a898da835
Anderson Martins <anderson@publishpress.com>
2023-12-15 10:48:46 -0300

    Remove not existent script from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 module.exports = {
     mode: NODE_ENV,
     entry: {
-        'modules/custom-status/lib/custom-status-block': './modules/custom-status/lib/custom-status-block.jsx',
         'modules/calendar/lib/async-calendar/js/index': './modules/calendar/lib/async-calendar/js/index.jsx',
         'modules/efmigration/lib/js/efmigration': './modules/efmigration/lib/js/efmigration.jsx'
     },
