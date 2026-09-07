71da817fff24f259f4d21c328f419fa413b4b649
Steve Burge <steve@rambleventures.com>
2026-09-07 08:36:53 -0400

    fix: add missing image alt attributes

--- modules/settings/views/footer-base.html.php
+++ modules/settings/views/footer-base.html.php
@@ -32,7 +32,7 @@ $rating_stars_markup = "
     </nav>
     <div class="pp-pressshack-logo">
         <a href="//publishpress.com" target="_blank" rel="noopener noreferrer">
-            <img src="<?php echo esc_url($context['plugin_url']); ?>common/img/publishpress-logo.png">
+            <img src="<?php echo esc_url($context['plugin_url']); ?>common/img/publishpress-logo.png" alt="<?php esc_attr_e('PublishPress', 'publishpress'); ?>">
         </a>
     </div>
 </footer>
